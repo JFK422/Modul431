fe4d45746a9f5f06e20a46e8d0cf5e7bb2c2f124
JFK422 <kenneth.mathis99@gmail.com>
2019-10-25 22:11:05 +0200

    Started commenting the code

--- Code/setup.py
+++ Code/setup.py
@@ -1,3 +1,6 @@
+#File used to "freeze" executable file for the currently crunning OS.
+#Currently there are builds for Win64 & Win32 premade
+
 from cx_Freeze import setup, Executable
 
 setup(name='Lernspiel',version='1.0',description='Ein Lernspiel',executables = [Executable("index.py")])
